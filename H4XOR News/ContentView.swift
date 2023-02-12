3c988f2a5ca81ce310466e49c7335c2917391563
Matthew <matthew04042@hotmail.com>
2023-02-13 00:12:18 +0800

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Matthew Cheung on 13/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
